d3aa257a472f9da714665017d70728cc7e75ecd1
unknown <craig.arseneau@gmail.com>
2012-01-24 21:53:09 -0500

    big change

--- GolfPool/Default2.aspx
+++ GolfPool/Default2.aspx
@@ -1,15 +1,14 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GolfPool.Master" AutoEventWireup="true" CodeBehind="Default2.aspx.cs" Inherits="GolfPool.Default2" %>
-<%@ Register assembly="Telerik.Web.UI" namespace="Telerik.Web.UI" tagprefix="telerik" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GolfPool.Master" AutoEventWireup="true"
+    CodeBehind="Default2.aspx.cs" Inherits="GolfPool.Default2" %>
+
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
 <asp:Content runat="server" ID="Content1" ContentPlaceHolderID="Content1">
-<asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
-    <h2>Standings
-    </h2>
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+   
     <div class="articles">
-
         <br />
-
-        <telerik:RadGrid ID="RadGrid1" runat="server">
+        <telerik:RadGrid ID="RadGrid1" runat="server" CellSpacing="0" GridLines="None">
         </telerik:RadGrid>
-
-</div>
+    </div>
 </asp:Content>
--- GolfPool/Default2.aspx.cs
+++ GolfPool/Default2.aspx.cs
@@ -12,12 +12,7 @@ namespace GolfPool
         protected void Page_Load(object sender, EventArgs e)
         {
             SqlConnection conn = new SqlConnection("Server=12a5e980-74a4-4c94-a480-9fd70124ef10.sqlserver.sequelizer.com;Database=db12a5e98074a44c94a4809fd70124ef10;User ID=dsscpeteldbsyyzj;Password=4SXf82Dtu5m6YCGKykANoz4SuZqo53EBjr2rLHdpnwTQ7Wt8HztVDphBWeThKpxA;");
-            SqlCommand cmd = new SqlCommand("select entrantname,SUM(winnings) as winnings from entrants "
-            + "left outer join picks on entrants.entrantid = picks.entrantid "
-            + "left outer join golfers on picks.golferid = golfers.golferid "
-            + "left outer join winnings on golfers.golferid = winnings.golferid "
-            + "group by entrantname "
-            + "order by winnings desc", conn);
+            SqlCommand cmd = new SqlCommand("select RANK() OVER (ORDER BY SUM(winnings) DESC) as entrantrank, entrantname,SUM(winnings) as winnings from entrants left outer join picks on entrants.entrantid = picks.entrantid left outer join golfers on picks.golferid = golfers.golferid left outer join winnings on golfers.golferid = winnings.golferid group by entrantname order by winnings desc", conn);
 
 
             conn.Open();
